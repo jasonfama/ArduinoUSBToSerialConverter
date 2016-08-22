e96f99f8ee7f307ee4ee70daf660161aaab91357
jasonfama <famajohnjason@yahoo.com>
2016-08-22 17:50:32 +0800

    Added description
    Added description as comments

---
+++ ArduinoUSBToSerialConverter.ino
@@ -0,0 +1,10 @@
+/* Cheap Arduino based USB to Serial Converter*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
